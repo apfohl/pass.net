a6c375757159806f044c4ebf8f0585656a98251a
Andreas Pfohl <mail@apfohl.com>
2021-11-01 22:23:22 +0100

    Functioning unlock screen

--- Pass/Components/Commands/RelayCommand.cs
+++ Pass/Components/Commands/RelayCommand.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Reactive;
+using System.Reactive.Linq;
 using System.Threading.Tasks;
 
 namespace Pass.Components.Commands
@@ -9,6 +11,12 @@ namespace Pass.Components.Commands
         private readonly Func<bool> canExecute;
 
         public RelayCommand(Func<Task> func, Func<bool> canExecute)
+            : this(func, canExecute, Observable.Never<Unit>())
+        {
+        }
+
+        public RelayCommand(Func<Task> func, Func<bool> canExecute, IObservable<Unit> canExecuteChanged)
+            : base(canExecuteChanged)
         {
             this.func = func;
             this.canExecute = canExecute;
@@ -17,6 +25,5 @@ namespace Pass.Components.Commands
         protected override async void OnExecute(object parameter) => await func();
 
         protected override bool OnCanExecute(object parameter) => canExecute();
-
     }
 }
--- Pass/ViewModels/UnlockViewModel.cs
+++ Pass/ViewModels/UnlockViewModel.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Windows.Input;
 using Pass.Components.Binding;
 using Pass.Components.Commands;
@@ -10,16 +11,25 @@ namespace Pass.ViewModels
     public record Unlocked(string Password);
 
     [View(typeof(UnlockView))]
-    public sealed class UnlockViewModel : Bindable
+    public sealed class UnlockViewModel : Bindable, IDisposable
     {
-        private readonly MessageBus messageBus;
+        private readonly ReactiveProperty<string> password = new(string.Empty);
+        private readonly RelayCommand unlock;
 
-        public string Password { get; set; }
+        public string Password
+        {
+            get => password.Value;
+            set => password.Value = value;
+        }
 
-        public ICommand Unlock => new RelayCommand(
-            () => messageBus.Publish(new Unlocked(Password)),
-            () => true);
+        public ICommand Unlock => unlock;
 
-        public UnlockViewModel(MessageBus messageBus) => this.messageBus = messageBus;
+        public UnlockViewModel(MessageBus messageBus) =>
+            unlock = new RelayCommand(
+                () => messageBus.Publish(new Unlocked(Password)),
+                () => !string.IsNullOrEmpty(Password),
+                password.Changed);
+
+        public void Dispose() => unlock.Dispose();
     }
 }
