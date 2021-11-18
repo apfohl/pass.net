906b1866a76efabf58b272aac7e171e8ee09b085
Andreas Pfohl <mail@apfohl.com>
2021-11-18 14:06:08 +0100

    Startup window sizing and MonadicBits 0.4.0

--- Pass/Components/Extensions/FunctionalExtensions.cs
+++ Pass/Components/Extensions/FunctionalExtensions.cs
@@ -10,14 +10,6 @@ using static Functional;
 
 public static class FunctionalExtensions
 {
-    public static IEnumerable<T> ToEnumerable<T>(this Maybe<T> @this) =>
-        @this.Match(Elevate, Enumerable.Empty<T>);
-
-    private static IEnumerable<T> Elevate<T>(this T @this)
-    {
-        yield return @this;
-    }
-
     public static Maybe<T> SingleOrNothing<T>(this IEnumerable<T> @this, Func<T, bool> predicate) =>
         @this.Where(predicate)
             .Select(i => i.Just())
